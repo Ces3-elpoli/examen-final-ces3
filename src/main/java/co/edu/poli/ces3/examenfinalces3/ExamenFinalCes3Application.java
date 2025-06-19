cf0b38e01ee311a983c176dd9b9ffcf943ce27a4
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-06-18 19:22:31 -0500

    Initial project setup and dependencies

---
+++ src/main/java/co/edu/poli/ces3/examenfinalces3/ExamenFinalCes3Application.java
@@ -0,0 +1,13 @@
+package co.edu.poli.ces3.examenfinalces3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalCes3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalCes3Application.class, args);
+    }
+
+}
---
+++ src/main/java/co/edu/poli/ces3/examenfinalces3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.edu.poli.ces3.examenfinalces3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExamenFinalCes3Application.class);
+    }
+
+}
